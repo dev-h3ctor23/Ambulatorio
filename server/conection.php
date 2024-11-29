0d59e4289b9966318d8ca821af3fef8ab8df0a39
dev-h3ctor23 <tapiah3ctor23@outlook.es>
2024-11-29 13:41:16 +0100

    0.3.2 - correcciones de tablas, inserciones de datos y validacion en el redireccionamiento

--- server/conection.php
+++ server/conection.php
@@ -44,13 +44,8 @@ $sql = "CREATE TABLE IF NOT EXISTS paciente (
     usuario_id INT(6) UNSIGNED NOT NULL,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
-    dni VARCHAR(30) NOT NULL UNIQUE,
     fecha_de_nacimiento DATE NOT NULL,
-    tipo_usuario ENUM('paciente') NOT NULL,
-    contraseña VARCHAR(50) NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (dni) REFERENCES usuarios(dni),
-    FOREIGN KEY (contraseña) REFERENCES usuarios(password)
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 )";
 $conn->query($sql);
 
@@ -60,14 +55,9 @@ $sql = "CREATE TABLE IF NOT EXISTS medico (
     usuario_id INT(6) UNSIGNED NOT NULL,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
-    dni VARCHAR(30) NOT NULL UNIQUE,
     fecha_de_nacimiento DATE NOT NULL,
-    tipo_usuario ENUM('medico') NOT NULL,
-    contraseña VARCHAR(50) NOT NULL,
     especialidad VARCHAR(50) NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (dni) REFERENCES usuarios(dni),
-    FOREIGN KEY (contraseña) REFERENCES usuarios(password)
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 )";
 $conn->query($sql);
 
@@ -85,29 +75,91 @@ if ($result->num_rows == 0) {
 
 // Insertar 20 usuarios, 10 pacientes y 10 médicos
 $insert_sql = "INSERT INTO usuarios (dni, password, tipo_usuario) VALUES 
-('11111111', 'password1', 'paciente'),
-('22222222', 'password2', 'paciente'),
-('33333333', 'password3', 'paciente'),
-('44444444', 'password4', 'paciente'),
-('55555555', 'password5', 'paciente'),
-('66666666', 'password6', 'paciente'),
-('77777777', 'password7', 'paciente'),
-('88888888', 'password8', 'paciente'),
-('99999999', 'password9', 'paciente'),
-('10101010', 'password10', 'paciente'),
-('12121212', 'password11', 'medico'),
-('13131313', 'password12', 'medico'),
-('14141414', 'password13', 'medico'),
-('15151515', 'password14', 'medico'),
-('16161616', 'password15', 'medico'),
-('17171717', 'password16', 'medico'),
-('18181818', 'password17', 'medico'),
-('19191919', 'password18', 'medico'),
-('20202020', 'password19', 'medico'),
-('21212121', 'password20', 'medico')";
+('45328901', '45328901', 'paciente'),
+('56437802', '56437802', 'paciente'),
+('67548903', '67548903', 'paciente'),
+('78659004', '67548903', 'paciente'),
+('89760105', '89760105', 'paciente'),
+('90871206', '90871206', 'paciente'),
+('01982307', '01982307', 'paciente'),
+('12093408', '12093408', 'paciente'),
+('23104509', '23104509', 'paciente'),
+('34215610', '34215610', 'paciente'),
+('45326711', '45326711', 'medico'),
+('56437812', '56437812', 'medico'),
+('67548913', '67548913', 'medico'),
+('78659014', '78659014', 'medico'),
+('89760115', '89760115', 'medico'),
+('90871216', '90871216', 'medico'),
+('01982317', '01982317', 'medico'),
+('12093418', '12093418', 'medico'),
+('23104519', '23104519', 'medico'),
+('34215620', '34215620', 'medico')
+
+ON DUPLICATE KEY UPDATE password=VALUES(password), tipo_usuario=VALUES(tipo_usuario)";
 $conn->query($insert_sql);
 
-echo "Tablas creadas y usuarios insertados exitosamente.";
+// Insertar datos en la tabla paciente
+$insert_sql = "INSERT INTO paciente (usuario_id, nombre, apellido, fecha_de_nacimiento) VALUES 
+((SELECT id FROM usuarios WHERE dni='45328901'), 'Juan', 'Pérez', '1987-03-15'),
+((SELECT id FROM usuarios WHERE dni='56437802'), 'María', 'Gómez', '1990-07-25'),
+((SELECT id FROM usuarios WHERE dni='67548903'), 'Luis', 'Martínez', '1992-11-02'),
+((SELECT id FROM usuarios WHERE dni='78659004'), 'Ana', 'Rodríguez', '1985-05-14'),
+((SELECT id FROM usuarios WHERE dni='89760105'), 'Carlos', 'López', '1995-08-08'),
+((SELECT id FROM usuarios WHERE dni='90871206'), 'Sofía', 'García', '1997-01-30'),
+((SELECT id FROM usuarios WHERE dni='01982307'), 'Diego', 'Hernández', '1994-04-10'),
+((SELECT id FROM usuarios WHERE dni='12093408'), 'Laura', 'Díaz', '1991-09-19'),
+((SELECT id FROM usuarios WHERE dni='23104509'), 'Andrés', 'Castro', '1989-02-22'),
+((SELECT id FROM usuarios WHERE dni='34215610'), 'Paula', 'Ramos', '1996-12-05')";
+$conn->query($insert_sql);
 
+// Insertar datos en la tabla medico
+$insert_sql = "INSERT INTO medico (usuario_id, nombre, apellido, fecha_de_nacimiento, especialidad) VALUES 
+((SELECT id FROM usuarios WHERE dni='45326711'), 'Miguel', 'Romero', '1975-06-12', 'Cardiología'),
+((SELECT id FROM usuarios WHERE dni='56437812'), 'Julia', 'Fernández', '1980-03-28', 'Dermatología'),
+((SELECT id FROM usuarios WHERE dni='67548913'), 'Álvaro', 'Santos', '1982-09-14', 'Neurología'),
+((SELECT id FROM usuarios WHERE dni='78659014'), 'Clara', 'Mendoza', '1978-01-05', 'Pediatría'),
+((SELECT id FROM usuarios WHERE dni='89760115'), 'Roberto', 'Silva', '1976-10-20', 'Psiquiatría'),
+((SELECT id FROM usuarios WHERE dni='90871216'), 'Elena', 'Ortiz', '1983-08-17', 'Ginecología'),
+((SELECT id FROM usuarios WHERE dni='01982317'), 'Ignacio', 'Moreno', '1977-04-22', 'Oncología'),
+((SELECT id FROM usuarios WHERE dni='12093418'), 'Teresa', 'Cruz', '1984-12-11', 'Urología'),
+((SELECT id FROM usuarios WHERE dni='23104519'), 'Felipe', 'Vega', '1981-02-03', 'Oftalmología'),
+((SELECT id FROM usuarios WHERE dni='34215620'), 'Mariana', 'Reyes', '1979-11-30', 'Gastroenterología')";
+$conn->query($insert_sql);
+
+// ? Conslta para verificar el tipo de usuario y redirigir a la página correspondiente
+    // * prepare: Prepara una sentencia SQL para ser ejecutada por el método execute()
+    // * bind_param: Une variables a una sentencia SQL
+    // * execute: Ejecuta la sentencia preparada
+    // * get_result: Obtiene un resultado de la sentencia preparada
+    // * ss: Tipo de datos de las variables que se van a unir a la sentencia SQL
+    $stmt = $conn->prepare("SELECT tipo_usuario FROM usuarios WHERE dni = ? AND password = ?"); // ! NO TOCAR: Funciona milagrosamente!
+    $stmt->bind_param("ss", $dni, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    // ? Verificamos si el usuario existe en la base de datos
+        // * fetch_assoc: Obtiene una fila de resultados como un array asociativo
+        // * echo '<script>window.location.href = "";</script>': Redirige a la página correspondiente con el echo de un script.
+    if($result->num_rows > 0) {
+        $row = $result->fetch_assoc();  
+        $tipo_usuario = $row['tipo_usuario']; 
+        if($tipo_usuario === 'paciente') {
+            // ! Redirigir a paciente.html
+            echo '<script>window.location.href = "../patient.html";</script>';
+        } elseif($tipo_usuario === 'medico') {
+            // ! Redirigir a medico.html
+            echo '<script>window.location.href = "../doctor.html";</script>';
+        } else {
+            // ! Redirigir a unknown-user.html
+            echo '<script>window.location.href = "unknown-user.html";</script>';
+        }
+    } else {
+            // ! Redirigir a unknown-user.html
+        echo '<script>window.location.href = "../unknown-user.html";</script>';
+    }
+
+// ? Cerramos la declaracion y la conexión a la base de datos
+$stmt->close();
 $conn->close();
 ?>
